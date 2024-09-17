38806f014f38d379106775b28e37741b41810f54
zeroxxl <zeroxxhls@gmail.com>
2024-09-17 20:39:09 +0200

    first commit

---
+++ src/data.js
@@ -0,0 +1,26 @@
+const tours = [
+    {
+      id: 1,
+      name: "Best of Paris in 7 Days Tour",
+      info: "The capital of France, Paris, is one of the main European cities and a world center of culture, art, fashion and gastronomy. The central part of the city, built in the 19th century, has wide boulevards and the Seine River. The most famous sights of Paris are the Eiffel Tower and Notre Dame Cathedral in the Gothic style, built in the 12th century. The city is also famous for its numerous cafes and boutiques with clothes from famous fashion designers on the Rue Faubourg Saint-Honoré. Experience the best of Paris in 7 days.",
+      image: 'https://images.unsplash.com/photo-1502602898657-3e91760cbb34',
+      price: "2,199",
+    },
+    {
+      id: 2,
+      name: "Explore Rome in 5 Days",
+      info: "Visit the Colosseum, Vatican, and taste the best Italian cuisine in this 5-day tour of Rome. Discover ancient history, breathtaking art, and vibrant local culture as you explore one of the world's most iconic cities.Embark on an unforgettable 5-day adventure through Rome, where history meets modern charm. Start with the grandeur of the Colosseum and the Roman Forum, stepping into the world of gladiators and emperors. Marvel at the awe-inspiring beauty of Vatican City, home to St. Peter’s Basilica and the Sistine Chapel. Wander through the vibrant streets of Trastevere, savor authentic Italian dishes, and sip on the finest espresso. Each day unveils a new layer of Rome’s rich culture, making this journey a perfect blend of history, art, and gastronomy.",
+      image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.theguardian.com%2Ftravel%2F2024%2Ffeb%2F18%2Frome-italy-why-spring-is-the-best-time-to-visit&psig=AOvVaw2E1_Xt5WFweHLheljsGkKD&ust=1726683426282000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMCEmM_LyogDFQAAAAAdAAAAABAJ',
+      price: "1,799",
+    },
+    {
+      id: 3,
+      name: "Adventure in the Swiss Alps",
+      info: "Enjoy breathtaking views and adventurous hikes in the Swiss Alps.Immerse yourself in the stunning beauty of the Swiss Alps on this 5-day adventure. Experience breathtaking views from majestic peaks and embark on exhilarating hikes through pristine landscapes. Discover charming alpine villages, savor local Swiss cuisine, and relax in serene mountain lodges. Whether you're seeking thrilling outdoor activities or peaceful nature escapes, the Swiss Alps offer an unforgettable blend of adventure and tranquility.",
+      image: "https://images.unsplash.com/photo-1593466581551-58b02f56843d",
+      price: "2,499",
+    },
+  ];
+  
+  export default tours;
+  
